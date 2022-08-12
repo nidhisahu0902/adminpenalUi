78c08a2aae906e9625640981ee6d51e695c96427
Nidhi Sahu <Nidhi.Sahu@securemeters.com>
2022-08-12 11:23:11 +0530

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ManageCategoryComponent } from './components/manage-category/manage-category.component';
+import { ManageItemComponent } from './components/manage-item/manage-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    DashboardComponent,
+    ContactUsComponent,
+    GalleryComponent,
+    ManageCategoryComponent,
+    ManageItemComponent
   ],
   imports: [
     BrowserModule,
